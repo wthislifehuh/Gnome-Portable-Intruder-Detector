41c11b5c6067f3d8d29d8d28294928eaf475cdd7
Ken How <kenhow03@gmail.com>
2024-08-19 02:42:23 +0800

    added video recording in camera_v2

--- modules/camera_v2.py
+++ modules/camera_v2.py
@@ -3,6 +3,7 @@ from flask import Response, stream_with_context
 from event_detector import EventDetector
 from object_detector import ObjectDetector
 import time
+import os
 
 
 class Camera:
@@ -11,6 +12,8 @@ class Camera:
         self.cap = None
         self.event_detector = EventDetector()
         self.object_detector = ObjectDetector()
+        self.is_recording = False
+        self.out = None
 
     def start_camera(self):
         self.cap = cv2.VideoCapture(self.camera_index)
@@ -86,6 +89,10 @@ class Camera:
                         ):  # Make sure that the notification is sent only after certain threshold
                             # Trigger notification module here!!!
                             print("Trigger intruder notification")
+
+                            # Trigger video recording
+                            if not self.is_recording:
+                                self.start_recording(self.cap)
                             person_last_notification_time = current_time
 
                 if result["is_animal"]:  # If animal is detected
@@ -106,7 +113,15 @@ class Camera:
                             print("Trigger animal notification")
                             animal_last_notification_time = current_time
 
+                # Continuously write frames to the video file while recording
+                if self.is_recording and self.out:
+                    self.out.write(frame)
+
             else:
+                intruder_counter = 0
+                animal_counter = 0
+                if self.is_recording:
+                    self.stop_recording()
                 print("No Event Detected in ROI")
 
             # Display the current frame
@@ -115,6 +130,30 @@ class Camera:
             # Check for user input to exit
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
+
+    def start_recording(self, cap, output_filename="output.mp4"):
+        # Define the path where the video will be saved
+        output_dir = os.path.join(os.getcwd(), "assets", "videos")
+        os.makedirs(output_dir, exist_ok=True)
+        output_filepath = os.path.join(output_dir, output_filename)
+
+        # Define the codec and create a VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec for .mp4 files
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+        self.out = cv2.VideoWriter(
+            output_filepath, fourcc, 20.0, (frame_width, frame_height)
+        )
+        self.is_recording = True
+        print("Recording started...")
+
+    def stop_recording(self):
+        if self.is_recording:
+            self.is_recording = False
+            if self.out:
+                self.out.release()
+                self.out = None
+            print("Recording stopped.")
 
     def generate_frame(self):
         while True:
