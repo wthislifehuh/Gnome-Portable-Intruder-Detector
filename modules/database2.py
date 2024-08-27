ae5ae9acb78479f7183977909f88093a4e1b1bd2
Joe Ee <2103160@1utar.my>
2024-08-28 00:26:35 +0800

    MongoDB database

--- modules/database2.py
+++ modules/database2.py
@@ -1,4 +1,4 @@
-# Cloud Firestore
+# Cloud Firestore =================================================================
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
